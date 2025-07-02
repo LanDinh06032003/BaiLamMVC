b19ba10c8985dd72190d6e6a0cab322db954eccb
“LanDinh06032003” <2121050498@student.humg.edu.vn>
2025-07-02 13:28:51 +0700

    Create_File_HeThongPhanPhoi

---
+++ BailamMVC/Models/HeThongPhanPhoi.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+namespace BailamMVC.Models;
+
+public class HeThongPhanPhoi
+{
+    public string? MaHTTP { get; set; }
+    public string? MaHTPP { get; set; }
+}
--- BailamMVC/Program.cs
+++ BailamMVC/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=HelloWorld}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
