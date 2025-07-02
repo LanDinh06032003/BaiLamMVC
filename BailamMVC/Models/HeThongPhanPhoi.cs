a2232c940a4637f0615ce0f7ed73308eea8224f8
“LanDinh06032003” <2121050498@student.humg.edu.vn>
2025-07-02 13:30:17 +0700

    Create_File_HeThongPhanPhoi

--- BailamMVC/Models/HeThongPhanPhoi.cs
+++ BailamMVC/Models/HeThongPhanPhoi.cs
@@ -4,5 +4,5 @@ namespace BailamMVC.Models;
 public class HeThongPhanPhoi
 {
     public string? MaHTTP { get; set; }
-    public string? MaHTPP { get; set; }
+    public string? TenHTPP { get; set; }
 }
