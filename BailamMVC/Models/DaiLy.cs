1c0a7596e2a69eec4c7629781cadc188830a9360
“LanDinh06032003” <2121050498@student.humg.edu.vn>
2025-07-02 13:45:04 +0700

    Create_File_DaiLy

---
+++ BailamMVC/Models/DaiLy.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BailamMVC.Models;
+
+public class DaiLy
+{
+    [Key]
+    public string? MaDaiLy { get; set; }
+    public string? TenDaiLy { get; set; }
+    public string? DiaChi { get; set; }
+    public string? NguoiDaiDien { get; set; }
+    public string? DienThoai { get; set; }
+    [ForeignKey("HeThongPhanPhoi")]
+    public string? MaHTTP { get; set; }
+    // dùng để thiết lập mối quan hệ giữa hai thực thể
+    public HeThongPhanPhoi? HeThongPhanPhoi { get; set; }
+}
--- BailamMVC/Models/HeThongPhanPhoi.cs
+++ BailamMVC/Models/HeThongPhanPhoi.cs
@@ -3,6 +3,10 @@ namespace BailamMVC.Models;
 
 public class HeThongPhanPhoi
 {
+    [Key]
     public string? MaHTTP { get; set; }
     public string? TenHTPP { get; set; }
+    
+    // Quan hệ 1 - N: Một HTPP có nhiều Đại lý
+    public List<DaiLy>? DaiLy { get; set; }
 }
